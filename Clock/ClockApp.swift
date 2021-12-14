6cefc73ab3dca99ff28b1baa1efe8f2ba6f83a00
dane <dane@rinckadvertising.com>
2021-12-14 13:55:04 -0500

    Initial Commit

---
+++ Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by Dane Johnson on 12/14/21.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Clock
+//
+//  Created by Dane Johnson on 12/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
